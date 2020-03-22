4a13ec231ce41ba337635c48da700c7579beed85
tetyorkin <tetyorkin@gmail.com>
2020-03-23 01:15:12 +0600

    del space dirty_main

--- 1.1 «Import. Module. Package»/dirty_main.py
+++ 1.1 «Import. Module. Package»/dirty_main.py
@@ -3,9 +3,7 @@ from application.db.people import *
 from datetime import *
 
 if __name__ == '__main__':
-    # Задание 1 и 2
     calculate_salary()
     get_employees()
-    # Задание 3
     print(f'{datetime.today():%Y-%B-%d}')
     print(datetime.now().strftime('%Y-%B-%d'))
