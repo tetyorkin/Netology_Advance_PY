13f7aacb12b2627e52e0118a91069eeb9c6f1748
tetyorkin <tetyorkin@gmail.com>
2020-03-26 00:18:45 +0600

    task1 Done

--- 1.2.Iterators.Generators.Yield/main.py
+++ 1.2.Iterators.Generators.Yield/main.py
@@ -2,31 +2,38 @@ import json
 from urllib.parse import quote
 
 
+class Country:
 
-def read_file(file_name):
-    with open(file_name) as f:
-        file_content = f.read()
-        templates = json.loads(file_content)
-        return templates
+    def __init__(self, path):
+        self.patch = path
+        self.file = open(self.patch)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        list_country = []
+        file_content = self.file.read()
+        try:
+            country_str = json.loads(file_content)
+            for country in country_str:
+                country_name = country['name']['common']
+                translations = country['translations']['rus']['common']
+                # ссылка формируется в ASCII, чтобы она была кликабельна
+                url_rus = f"https://ru.wikipedia.org/wiki/{quote(translations)}"
+                list_country.append(f'{country_name} - {url_rus}')
+        except json.decoder.JSONDecodeError:
+            raise StopIteration
+        return list_country
 
 
-def find_country(file_name):
-    contry
-    list_country = []
-    for country in read_file(file_name):
-        country_name = country['name']['common']
-        translations = country['translations']['rus']['common']
-        url_rus = f"https://ru.wikipedia.org/wiki/{quote(translations)}"
-        list_country.append(f'{country_name:<20} - {url_rus}')
-    return list_country
-
-
-def write_file(file_name):
+def write_file(data_in):
     with open('countries_url.txt', 'w', encoding='utf-8') as file:
-        data_raw = ',  '.join(find_country(file_name))
+        data_raw = ',  '.join(data_in)
         data = data_raw.replace(',  ', '\n')
         file.write(str(data))
 
 
 if __name__ == '__main__':
-    write_file('countries.json')
+    url_country = Country('countries.json')
+    write_file(url_country.__next__())
