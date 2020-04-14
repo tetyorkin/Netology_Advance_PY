69915ecab9cca81401fc975860a2acf2f2b4ddac
tetyorkin <tetyorkin@gmail.com>
2020-04-14 19:14:49 +0600

    remake all functions

--- 2.2.Regexp/main.py
+++ 2.2.Regexp/main.py
@@ -1,32 +1,51 @@
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
 import re
-from pprint import pprint
 
-with open("phonebook_raw.csv", encoding='utf-8') as f:
-    rows = csv.reader(f, delimiter=",")
+pattern = re.compile(r'(\+7|8){1}\s*\(?(\d{3})\)?(\s*|-*)*(\d{3})(\s*|-*)*(\d{2})(\s*|-*)*'
+                     r'(\d{2})(\s*\(*(доб.)(\s)*(\d+))?\)*')
+
+with open("phonebook_raw.csv", encoding='utf8') as f:
+    rows = csv.reader(f, delimiter=',')
     contacts_list = list(rows)
-# pprint(contacts_list)
 
 
-# TODO 1: выполните пункты 1-3 ДЗ
-def phone(contact_list):
-    last_name = re.compile(r"\['(?P<last_name>\w+)(',\n\s+'|\s|'.\s')(?P<fist_name>firs\w+|[А-я]+)(',\n\s+'|\s|'.\s')"
-                           r"(?P<surname>sur\w+|[А-я]+|)(',\n\s+'',\n\s+'',\n\s+'|',\s'', '', '|', '', '|',\n\s+')"
-                           r"(?P<organization>\w+|)(',\n\s+'|)(?P<position>pos\w+|\w+.+\n\s+.+)", re.M)
-    list_last_name = []
-    for match in last_name.finditer(str(contact_list)):
-        list_last_name += [match.group('last_name'), match.group('fist_name'), match.group('surname'),
-                           match.group('organization'), match.group('position')]
-    print(list_last_name)
+def check_name(last_name, first_name, list_of_items):
+    for item in list_of_items:
+        if item[0] == last_name and item[1] == first_name:
+            return list_of_items.index(item)
 
 
-# TODO 2: сохраните получившиеся данные в другой файл
-# код для записи файла в формате CSV
-with open("phonebook.csv", "w", encoding='utf-8') as f:
+def pretty_phone(phone, pattern):
+    return re.sub(pattern, r'+7(\2)\4-\6-\8\11\10\12', phone)
+
+
+def contacts():
+    new_contacts = [contacts_list[0]]
+    contacts_list.pop(0)
+    for contact in contacts_list:
+        full_name = ' '.join(contact[0:3])
+        list_full_name = full_name.split(' ')
+        checked_name = check_name(list_full_name[0], list_full_name[1], new_contacts)
+        pretty_number = pretty_phone(contact[5], pattern)
+        new_contact = [list_full_name[0],
+                       list_full_name[1],
+                       list_full_name[2],
+                       contact[3],
+                       contact[4],
+                       pretty_number,
+                       contact[6]]
+        if checked_name is None:
+            new_contacts.append(new_contact)
+        else:
+            i = 0
+            while i < len(new_contacts[checked_name]):
+                if new_contacts[checked_name][i] == '':
+                    new_contacts[checked_name][i] = new_contact[i]
+                i += 1
+    return new_contacts
+
+
+with open("phonebook.csv", "w", encoding='utf8', newline='') as f:
     datawriter = csv.writer(f, delimiter=',')
-    # Вместо contacts_list подставьте свой список
-    datawriter.writerows(contacts_list)
-
-if __name__ == '__main__':
-    phone(contacts_list)
+    datawriter.writerows(contacts())
